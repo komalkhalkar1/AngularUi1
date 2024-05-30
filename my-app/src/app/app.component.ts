b646ead9711725f75ed79269c808e2aecd682819
unknown <komalkhalkar2255@gmail.com>
2024-05-30 16:27:09 +0530

    first commit

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'my-app';
+  readonly APIUrl="http://localhost:5038/api/todoapp/";
+
+  constructor(private http:HttpClient){}
+  notes:any=[];
+
+  refreshNotes(){
+    this.http.get(this.APIUrl+'GetNotes').subscribe(data=>{
+      this.notes=data;
+    })
+
+  }
+  ngOnInit(){
+    this.refreshNotes();
+  }
+  addNotes(){
+    var newNotes=(<HTMLInputElement>document.getElementById("newNotes")).value;
+    var formData=new FormData();
+    formData.append("newNotes",newNotes);
+    this.http.post(this.APIUrl+'AddNotes',formData).subscribe(data=>{
+      alert(data);
+      this.refreshNotes();
+    })
+  }
+
+  deleteNotes(id:any){
+    
+    this.http.delete(this.APIUrl+'DeleteNotes?id='+id).subscribe(data=>{
+      alert(data);
+      this.refreshNotes();
+    })
+  }
+}
